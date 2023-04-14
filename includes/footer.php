12cffcf6eb005cb2480f258fa95526e6349edfb7
Angel <63896621+angellhj@users.noreply.github.com>
2023-04-15 01:34:01 +0800

    complete index page frontend

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+<?php echo
+
+'<footer class="footer">
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-xs-6">
+            <h6>CONNECT WITH US</h6>
+            <ul class="social-icons list-unstyled ">
+                <li class="d-inline-block"><a class="facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                <li class="d-inline-block"><a class="whatsapp" href="#"><i class="fab fa-whatsapp"></i></a></li>
+                <li class="d-inline-block"><a class="instagram" href="#"><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+            <h6>EXPLORE</h6>
+            <ul class="footer-links list-unstyled">
+                <li><a class="footer-link" href="templates/contact_us.html">Contact Us</a></li>
+                <li><a class="footer-link" href="templates/about_us.html">About Us</a></li>
+                <li><a class="footer-link" href="templates/privacy_policy.html">Privacy Policy</a></li>
+                <li><a class="footer-link" href="templates/terms_condition.html">Terms and Conditions</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+            <h6>ADDRESS</h6>
+            <p>E-G-25, Gala Street Mall,<br>Jalan Tun Jugah,<br>93350 Kuching,<br>Sarawak</p>
+        </div>
+
+        <div class="col-md-3 col-xs-6">
+            <h6>OFFICE HOURS</h6>
+            <table class="table table-borderless text-white">
+                <tbody>
+                    <tr>
+                        <td>Monday - Friday:</td>
+                        <td>9:00 AM - 5:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Saturday:</td>
+                        <td>10:00 AM - 2:00 PM</td>
+                    </tr>
+                    <tr>
+                        <td>Sunday:</td>
+                        <td>Closed</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <hr class="bg-light">
+    <div class="row text-center">
+        <div class="col-md-12 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2023 All Rights Reserved by 
+                <a class="text-white font-weight-bold" href="#">Good Times Travel</a>.
+            </p>
+        </div>
+    </div>
+</div>
+</footer>';
+
+?>
