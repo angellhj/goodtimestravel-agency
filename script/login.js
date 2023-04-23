bc8e636a4ff9f02d4d420b656d9f5c0e32b2652c
Angel <63896621+angellhj@users.noreply.github.com>
2023-04-24 01:23:15 +0800

    login form validation issue

--- script/login.js
+++ script/login.js
@@ -1,27 +1,26 @@
 var app = angular.module('myApp', []);
 
-app.controller('LoginController', function($scope) {
-	$scope.username = '';
-	$scope.password = '';
-	$scope.email = '';
+app.controller('loginCtrl', function($scope, $window) {
 	$scope.error = '';
-
-	$scope.loginForm = function() {
-		if ($scope.username === 'admin' && $scope.password === 'password') {
+	$scope.login = function() {
+		if ($scope.username == 'admin' && $scope.password == 'admin') {
+			console.log('login success'); // for testing
 			alert('Login successful!');
+			$window.location.href = "../index.html";
 		} else {
+			console.log('login failed'); // for testing
 			$scope.error = 'Incorrect username or password.';
 		}
 	};
 
-	// $scope.forgotPasswordForm = function() {
-	// 	// Add your forgot password logic here
-	// 	if ($scope.email !== 'example@example.com') {
-	// 		$scope.error = 'Email address not found.';
-	// 		return;
-	// 	}
-	// 	alert('An email has been sent with instructions to reset your password.');
-	// };
+	$scope.forgotPassword = function() {
+		if ($scope.email !== 'example@example.com') {
+			console.log('email unavailable'); // for testing
+			$scope.error = 'Email address not found.';
+			return;
+		}
+		alert('An email has been sent with instructions to reset your password.');
+	};
 });
 
 // app.controller('ForgotPasswordController', function($scope) {
