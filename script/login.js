d9426b430d5226d7902365a7687a9aaa1fe2c2b6
Angel <63896621+angellhj@users.noreply.github.com>
2023-04-25 03:43:42 +0800

    update login validation

--- script/login.js
+++ script/login.js
@@ -1,56 +1,27 @@
 var app = angular.module('myApp', []);
 
-app.controller('loginCtrl', function($scope, $window) {
-	$scope.error = '';
-	$scope.login = function() {
-		if ($scope.username == 'admin' && $scope.password == 'admin') {
-			console.log('login success'); // for testing
-			alert('Login successful!');
-			$window.location.href = "../index.html";
-		} else {
-			console.log('login failed'); // for testing
-			$scope.error = 'Incorrect username or password.';
+app.controller('LoginCtrl', function($scope) {
+	$scope.submitForm = function() {
+		// Add your login logic here
+		if ($scope.username !== 'admin' || $scope.password !== 'password') {
+			$scope.error = 'Invalid username or password.';
+			return;
 		}
 	};
+});
 
-	$scope.forgotPassword = function() {
-		if ($scope.email !== 'example@example.com') {
-			console.log('email unavailable'); // for testing
+app.controller('ForgotPasswordCtrl', function($scope) {
+	$scope.submitForm = function() {
+		// Add your forgot password logic here
+		if ($scope.email != 'test@email.com') {
 			$scope.error = 'Email address not found.';
 			return;
 		}
 		alert('An email has been sent with instructions to reset your password.');
+		$scope.dismissModal();
 	};
+
+    $scope.dismissModal = function() {
+        $('#forgotModal').modal('hide');
+    }
 });
-
-// app.controller('ForgotPasswordController', function($scope) {
-// 	$scope.email = '';
-// 	$scope.error = '';
-
-// 	$scope.submitForm = function() {
-// 		// Add your forgot password logic here
-// 		if ($scope.email !== 'example@example.com') {
-// 			$scope.error = 'Email address not found.';
-// 			return;
-// 		}
-// 		alert('An email has been sent with instructions to reset your password.');
-// 	};
-
-// 	$scope.showModal = false;
-// 	$scope.showForgotPasswordModal = function() {
-// 		$scope.showModal = true;
-// 	};
-// 	$scope.closeForgotPasswordModal = function() {
-// 		$scope.showModal = false;
-// 	};
-
-// 	$scope.submitFormtest = function() {
-// 		// Add your forgot password logic here
-// 		if ($scope.email !== 'example@example.com') {
-// 			$scope.error = 'Email address not found.';
-// 			return;
-// 		}
-// 		alert('An email has been sent with instructions to reset your password.');
-// 		$scope.closeForgotPasswordModal();
-// 	};
-// });
